22c70a0dddcc07f62865b0307691fb0b95bcc276
AndreaManuppelli <andreamanuppelli@gmail.com>
2025-01-23 06:33:43 +0100

    Added preline and tailwindcss updates, removed postcss

---
+++ RoboQBO4/tailwind.config.js
@@ -0,0 +1,30 @@
+import getPalette from "tailwindcss-palette-generator";
+
+
+
+const palette = getPalette([
+  {
+    color: "#0d3b66",
+    name: "primary",
+    shades: [100, 200, 300, 400, 450, 500, 600, 700, 800, 900]
+  },
+]);
+
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  [
+    "./node_modules/preline/preline.js",
+    "./src/**/*.{html,ts}"],
+    theme: {
+      extend: {
+        colors: palette,
+        
+      },
+      plugins: [
+        require('preline/plugin'),
+    ],
+}
+
+}
