08efc01eebf46cd3d1eda848fb8a67bcb75796d1
AndreaManuppelli <andreamanuppelli@gmail.com>
2025-02-04 17:57:46 +0100

    Added dynamic recipe table and styling

--- RoboQBO4/src/app/components/processing-table/processing-table.component.ts
+++ RoboQBO4/src/app/components/processing-table/processing-table.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ProcessingTableComponent {
 
+  recipes = Array.from({length: 20}, (_, i) => ({
+    id: i + 1,
+    name: ["Cappuccino", "Latte", "Espresso"][i % 3],
+    status: i === 0 ? "In attesa" : "Completato",
+    date_send: new Date(),
+    date_end: new Date(),
+  }));
 }
